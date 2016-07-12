bc7de9147f4d01c2c232611465b89c8b3b6dc595
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2016-07-12 17:25:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+
+const {
+    app
+} = electron;
+
+const {
+    BrowserWindow
+} = electron;
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
